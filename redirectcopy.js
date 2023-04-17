b4f734441e45a70a1552eb25b5db1445ff093856
mrtodesigner <mrtodesigner@gmail.com>
2023-04-17 10:26:33 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://faceseo.vn'>http://faceseo.vn</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='https://phunsuong.com.vn'>https://phunsuong.com.vn</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
